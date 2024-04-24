936b657ee4c0a5934c2c9a6c9f0afbdba559771d
sadiquex <abubakasaddik1@gmail.com>
2024-04-24 12:11:28 +0000

    updated cors policy

--- server/index.js
+++ server/index.js
@@ -5,13 +5,15 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(
-  cors({
-    origin: true, // allow requests from any origin
-    // credentials: true,
-    optionSuccessStatus: 200,
-  })
-);
+// app.use(
+//   cors({
+//     origin: true, // allow requests from any origin
+//     // credentials: true,
+//     optionSuccessStatus: 200,
+//   })
+// );
+app.use(cors());
+
 app.use(bodyParser.json());
 const port = process.env.PORT || 5000; // port to run server on
 
